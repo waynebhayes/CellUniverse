aa89b9bf075869cc79beb91f3036a487e0a87583
Shane Stahlheber <shane.stahlheber@gmail.com>
2017-04-06 13:25:55 -0700

    Code cleanup and add comments to explain collision math.

--- collisionEventsHandler.py
+++ collisionEventsHandler.py
@@ -3,37 +3,46 @@ from Bacterium import *
 # circle-circle collisions
 def circles_collision(B, i, j, circle_i, circle_j):
     d = np.linalg.norm(circle_j - circle_i)
+
+    # if the circles are overlapping
     if d < 2*B[i].radius:
+
         B[i].collided = True
         B[j].collided = True
+
+        # amount of overlap
         x = 2*B[i].radius - d
 
-        # bacterium i
+        # compute spring force acting on bacterium i
         F = B[i].k*x*normalize(circle_i - circle_j)
-        dp = F*dt
 
-            # linear movement
+        # change in momentum
+        dp = F*dt
         B[i].v += dp/B[i].m
 
-            # rotation movement
+        # moment of intertia
         inertia = B[i].m*B[i].length**2/12
+
+        # change in angular velocity
         dw = np.cross(circle_i - B[i].pos, dp)/inertia
         B[i].w += dw
 
-        
-        # bacterium j
+        # equal and opposite force acting on bacterium j
         F = -F
-        dp = F*dt
 
-            # linear movement
+        # change in momentum
+        dp = F*dt
         B[j].v += dp/B[j].m
         
-            # rotation movement
+        # moment of intertia
         inertia = B[j].m*B[j].length**2/12
+
+        # change in angular velocity
         dw = np.cross(circle_j - B[j].pos, dp)/inertia
         B[j].w += dw
 
         return True
+
     return False
         
 # circle-line collisions
@@ -42,33 +51,46 @@ def circle_line(B, i, j, circle_i, line_j):
     y = circle_i[1]
     m = line_j.m
     b = line_j.b
-    
-    d = abs(m*x - y + b)/sqrt(m**2 + 1)
+
+    # point on the line nearest the circle's center
     ip = np.array([(x + m*y - m*b)/(m**2 + 1), (m*x + m**2*y + b)/(m**2 + 1), 0])
 
+    # distance between the circle's center and nearest point on the line
+    d = abs(m*x - y + b)/sqrt(m**2 + 1)
+
+    # if the circle is overlapping the line,
+    #   and the nearest point is within the line segment
     if d < B[i].radius and np.dot(line_j.p1 - ip, line_j.p2 - ip) < 0:
+
         B[i].collided = True
         B[j].collided = True
+
+        # the amount of overlap
         x = B[i].radius - d
 
-        # bacterium i
+        # compute spring force acting on bacterium i
         F = B[i].k*x*(line_j.normal_vector)
+        
+        # linear movement
         dp = F*dt
-
-            # linear movement
         B[i].v += dp/B[i].m
 
-            # rotation movement
+        # moment of inertia
         inertia = B[i].m*B[i].length**2/12
+        
+        # change in angular velocity
         dw = np.cross(circle_i - B[i].pos, dp)/inertia
         B[i].w += dw
 
-        # bacterium j (only linear movement)
+        # equal and opposite force acting on bacterium j
         F = -F
+
+        # change in momentum
         dp = F*dt
         B[j].v += dp/B[j].m
 
         return True
+
     return False
 
 
@@ -93,10 +115,14 @@ def spin(bacterium):
 # Check for collision and move all collided bacteria 
 def run(B, M):
     for p in range(3):
+
         collided = False
+
         for i in range(len(M)):
             for j in range(i+1, len(M)):
-                if not M[i][j]: continue
+
+                if not M[i][j]: 
+                    continue
             
                 # circle-circle collisions
                 collided |= circles_collision(B, i, j, B[i].head_pos, B[j].head_pos)
@@ -114,11 +140,14 @@ def run(B, M):
                 collided |= circle_line(B, j, i, B[j].tail_pos, B[i].line_1)
                 collided |= circle_line(B, j, i, B[j].tail_pos, B[i].line_2)
 
-        if not collided: break
+        if not collided: 
+            break
+
         for bacterium in B:
             if bacterium.collided:
                 move(bacterium)
                 spin(bacterium)
+
                 bacterium.update()
             
             bacterium.collided = False
@@ -128,9 +157,13 @@ def run(B, M):
 # Move only bacterium i
 def run2(B, i, M):
     for p in range(3):
+
         collided = False
+
         for j in range(len(B)):
-            if not M[i][j]: continue
+
+            if not M[i][j]: 
+                continue
             
             # circle-circle collisions
             collided |= circles_collision(B, i, j, B[i].head_pos, B[j].head_pos)
@@ -150,31 +183,12 @@ def run2(B, i, M):
 
             B[j].collided = False
 
-        if not collided: break
+        if not collided: 
+            break
+
         move(B[i])
         spin(B[i])
+
         B[i].update()
         B[i].collided = False
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-    
-
