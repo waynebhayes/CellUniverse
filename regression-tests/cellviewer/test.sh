31a2abcb1933866395eb617855e83750440a3c1b
waynebhayes <whayes@uci.edu>
2019-06-14 06:00:50 -0700

    minor fix to cellviewer regr test

--- regression-tests/cellviewer/test.sh
+++ regression-tests/cellviewer/test.sh
@@ -8,7 +8,7 @@ TEST_DIR=./regression-tests/cellviewer
 
 mkdir -p $TEST_DIR/test/
 rm -f $TEST_DIR/test/*
-cp -r $TEST_DIR/original_data/ $TEST_DIR/test/
+cp -r $TEST_DIR/original_data/* $TEST_DIR/test/
 
 mkdir -p $TEST_DIR/results/
 rm -f $TEST_DIR/results/*
@@ -27,4 +27,3 @@ python "$TEST_DIR/checkerCellViewer.py" \
     "-wd=$TEST_DIR"
 
 echo "Done testing cellviewer"
-exit
