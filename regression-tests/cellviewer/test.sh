eefda25b818fcd04080a909ba1731cabf4be55cd
waynebhayes <whayes@uci.edu>
2019-06-19 12:31:58 -0700

    minor mods #2 to cellview regression, but still breaks

--- regression-tests/cellviewer/test.sh
+++ regression-tests/cellviewer/test.sh
@@ -4,7 +4,7 @@ die() { echo "$@" >&2; exit 1; }
 echo "Testing cellviewer"
 
 TEST_DIR=./regression-tests/cellviewer
-[ -d "$TEST_DIR" ] || die "Must run from the repository's root directory!"
+[ -d "$TEST_DIR" ] || die "Must run from the repository's root directory"
 
 rm -rf $TEST_DIR/test
 mkdir $TEST_DIR/test
@@ -13,12 +13,8 @@ cp -r $TEST_DIR/original_data/* $TEST_DIR/test
 rm -rf $TEST_DIR/results
 mkdir $TEST_DIR/results
 
-if python3 "./cellviewer/radialtree.py" $TEST_DIR/test; then
-    :
-else
-    die "Python quit unexpectedly!"
-fi
+python3 "./cellviewer/radialtree.py" $TEST_DIR/test || die "Python died on radialtree.py unexpectedly"
 
-python3 "$TEST_DIR/checkerCellViewer.py" "-checkLevel=1" "-wd=$TEST_DIR"
+python3 "$TEST_DIR/checkerCellViewer.py" "-checkLevel=1" "-wd=$TEST_DIR" || die "Python died on checkerCellViewer.py"
 
 echo "Done testing cellviewer"
