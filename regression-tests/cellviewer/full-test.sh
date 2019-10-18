3d1f27cf9637326c925e69136b0b010834441ed5
Meta Novitia <mnovitia@uci.edu>
2019-10-17 22:24:13 -0700

    full-run regression

---
+++ regression-tests/cellviewer/full-test.sh
@@ -0,0 +1,20 @@
+npm install --prefix cellviewer
+
+rm -rf ./cellviewer/src/output/
+mkdir ./cellviewer/src/output/
+
+python "./main.py" \
+    --start 0 \
+    --finish 20 \
+    --debug "./debug" \
+    --input "./input/frame%03d.png" \
+    --output "./cellviewer/src/output" \
+    --config "./config.json" \
+    --initial "./cells.0.csv" \
+    --temp 10 \
+    --endtemp 0.01
+
+python "./cellviewer/radialtree.py" \
+    "./cellviewer/src/output"
+
+npm run --prefix cellviewer deploy
