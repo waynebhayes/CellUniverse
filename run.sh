b2b4ae2a13530158c8fa8829b0afb66658d04f07
Meta Novitia <mnovitia@uci.edu>
2020-01-21 14:48:28 -0800

    revert code

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-python3 "./main.py" \
+python ".\main.py" \
     --start 0 \
     --finish 60 \
-    --debug "./debug" \
-    --input "./input/frame%03d.png" \
-    --output "./output" \
-    --config "./config.json" \
-    --initial "./cells.0.csv" \
+    --debug ".\debug" \
+    --input ".\input\frame%03d.png" \
+    --output ".\output" \
+    --config ".\config.json" \
+    --initial ".\cells.0.csv" \
     --temp 10 \
     --endtemp 0.01
