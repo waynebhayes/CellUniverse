ad79384f63bdfde0627c675adda079a1d09ac999
GuanWen Qiu(Perry) <guanwenq@uci.edu>
2020-05-16 15:36:09 -0700

    update regression tests

--- regression-tests/graysynthetic/test.sh
+++ regression-tests/graysynthetic/test.sh
@@ -2,21 +2,25 @@
 die() { echo "$@" >&2; exit 1;
 }
 
-echo "Testing simulated annealing"
+echo "Testing graysynthetic image"
 
 [ -d "$REG_DIR" ] || die "Must run from the repository's root directory!"
 
 # create the test output dir if it doesn't exist
-mkdir -p $REG_DIR/output
-rm -f $REG_DIR/output/*
+rm -rf $REG_DIR/output
+mkdir $REG_DIR/output
+
+rm -rf $REG_DIR/bestfit
+mkdir $REG_DIR/bestfit
 
 if python3 "./main.py" \
     --frame_first 0 \
     --frame_last 13 \
     --debug "./debug" \
     --input "./input_gray/frame%03d.png" \
-    --graysynthetic True \
+    --graySynthetic \
     --output "$REG_DIR/output" \
+    --bestfit "$REG_DIR/bestfit" \
     --config "./config.json" \
     --initial "./cells.0.csv" ; then
     :
