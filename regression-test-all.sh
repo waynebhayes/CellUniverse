43a85c64d1609745472353f5ef89e0e3ce78fd38
waynebhayes <whayes@uci.edu>
2020-11-02 13:00:07 -0800

    Adding git-at so we don't uselessly run regressions on Jenkins

--- regression-test-all.sh
+++ regression-test-all.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
-RETURN=0
-#renice 19 $$
+if [ -f git-at -a `git log -1 --format=%at` -eq `tail -1 git-at` ]; then
+    echo -n "Repo unchanged; returning same status code as "
+    tail -1 git-at | xargs -I{} date -d @{} +%Y-%m-%d-%H:%M:%S
+    exit `head -1 git-at`
+fi
+
+git submodule init && git submodule update && (cd NetGO && git checkout master && git pull)
+nice -19 ./regression-test-all.sh -make
+(echo $?; git log -1 --format=%at) > git-at
+
+
+NUM_FAILS=0
 STDBUF=''
 if which stdbuf >/dev/null; then
 	STDBUF='stdbuf -oL -eL'
 fi
 for dir in regression-tests/*; do
-	REG_DIR=$dir
-	export REG_DIR
-	echo --- in directory $dir ---
-	for r in $dir/*.sh; do
-		echo --- running test $r ---
-		if eval $STDBUF "$r"; then # force output and error to be line buffered
-			:
-		else
-			RETURN=1
-		fi
-	done
+    REG_DIR=$dir
+    export REG_DIR
+    echo --- in directory $dir ---
+    for r in $dir/*.sh; do
+	echo --- running test $r ---
+	if eval $STDBUF "$r"; then # force output and error to be line buffered
+	    :
+	else
+	    (( NUM_FAILS+=$? ))
+	fi
+    done
 done
-exit $RETURN
+(echo $NUM_FAILS; git log -1 --format=%at) > git-at
+exit $NUM_FAILS
