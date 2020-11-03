b7ae3c7eb2ddabfb0b3117dd4bba0ab17f3b7e5b
waynebhayes <whayes@uci.edu>
2020-11-02 23:37:04 -0800

    update regression

--- regression-test-all.sh
+++ regression-test-all.sh
@@ -1,32 +1,41 @@
-#!/bin/sh
-if [ -f git-at -a `git log -1 --format=%at` -eq `tail -1 git-at` ]; then
+#!/bin/bash
+if [ -f git-at ] && [ `wc -l < git-at` -eq 2 -a `git log -1 --format=%at` -eq `tail -1 git-at` ]; then
     echo -n "Repo unchanged; returning same status code as "
     tail -1 git-at | xargs -I{} date -d @{} +%Y-%m-%d-%H:%M:%S
     exit `head -1 git-at`
 fi
+USAGE="USAGE: $0 [ list of tests to run, defaults to regression-tests/*/*.sh ]"
+source ~/bin/misc.sh
+PATH=`pwd`:`pwd`/scripts:$PATH
+export PATH
 
-git submodule init && git submodule update && (cd NetGO && git checkout master && git pull)
-nice -19 ./regression-test-all.sh -make
-(echo $?; git log -1 --format=%at) > git-at
-
+CORES=${CORES:=`cpus 2>/dev/null | awk '{c2=int($1/2); if(c2>0)print c2; else print 1}'`}
+[ "$CORES" -gt 0 ] || die "can't figure out how many cores this machine has"
+echo "Using $MAKE_CORES cores to make and $CORES cores for regression tests"
+export EXE CORES MAKE_CORES
 
 NUM_FAILS=0
 STDBUF=''
 if which stdbuf >/dev/null; then
-	STDBUF='stdbuf -oL -eL'
+    STDBUF='stdbuf -oL -eL'
 fi
-for dir in regression-tests/*; do
-    REG_DIR=$dir
+if [ $# -eq 0 ]; then
+    set regression-tests/*/*.sh
+fi
+for r
+do
+    REG_DIR=`dirname "$r"`
+    NEW_FAILS=0
     export REG_DIR
-    echo --- in directory $dir ---
-    for r in $dir/*.sh; do
-	echo --- running test $r ---
-	if eval $STDBUF "$r"; then # force output and error to be line buffered
-	    :
-	else
-	    (( NUM_FAILS+=$? ))
-	fi
-    done
+    echo --- running test $r ---
+    if eval $STDBUF "$r"; then # force output and error to be line buffered
+	:
+    else
+	NEW_FAILS=$?
+	(( NUM_FAILS+=$NEW_FAILS ))
+    fi
+    echo --- test $r incurred $NEW_FAILS failures, cumulative failures is $NUM_FAILS ---
 done
+echo Total number of failures: $NUM_FAILS
 (echo $NUM_FAILS; git log -1 --format=%at) > git-at
 exit $NUM_FAILS
