2fbaba8461947f2180447b3331f527ef1b9c256c
waynebhayes <whayes@uci.edu>
2020-11-29 22:17:30 +0000

    banish misc.sh and include functions inline in scripts

--- regression-test-all.sh
+++ regression-test-all.sh
@@ -1,4 +1,44 @@
 #!/bin/bash
+# Functions
+die(){ (echo "$USAGE"; echo "FATAL ERROR: $@")>&2; exit 1; }
+warn(){ (echo "WARNING: $@")>&2; }
+not(){ if eval "$@"; then return 1; else return 0; fi; }
+newlines(){ awk '{for(i=1; i<=NF;i++)print $i}' "$@"; }
+parse(){ awk "BEGIN{print $@}" </dev/null; }
+cpus() {
+    TMP=/tmp/cpus.$$
+    trap "/bin/rm -f $TMP; exit" 0 1 2 3 15
+
+    # Most Linux machines:
+    lscpu >$TMP 2>/dev/null && awk '/^CPU[(s)]*:/{cpus=$NF}END{if(cpus)print cpus; else exit 1}' $TMP && return
+
+    # MacOS:
+    ([ `arch` = Darwin -o `uname` = Darwin ] || uname -a | grep Darwin >/dev/null) && sysctl -n hw.ncpu && return
+
+    # Cygwin:
+    case `arch` in
+    CYGWIN*) grep -c '^processor[ 	]*:' /proc/cpuinfo; return ;;
+    *) if [ -d /dev/cpu -a ! -f /dev/cpu/microcode ]; then
+	ls -F /dev/cpu | fgrep -c
+	return
+       fi
+	;;
+    esac
+
+    # Oops
+    echo "couldn't figure out number of CPUs" >&2; exit 1
+}
+
+# generally useful Variables
+NL='
+'
+TAB='	'
+
+# Temporary Filename + Directory (both, you can use either, note they'll have different random stuff in the XXXXXX part)
+TMP=`mktemp /tmp/$BASENAME.XXXXXX`
+TMPDIR=`mktemp -d /tmp/$BASENAME.XXXXXX`
+trap "/bin/rm -rf $TMP $TMPDIR; exit" 0 1 2 3 15 # call trap "" N to remove the trap for signal N
+
 case "$1" in
 -use-git-at)
     if [ -f git-at ] && [ `wc -l < git-at` -eq 2 -a `git log -1 --format=%at` -eq `tail -1 git-at` ]; then
@@ -17,8 +57,8 @@ export PATH
 
 CORES=${CORES:=`cpus 2>/dev/null | awk '{c2=int($1/2); if(c2>0)print c2; else print 1}'`}
 [ "$CORES" -gt 0 ] || die "can't figure out how many cores this machine has"
-echo "Using $MAKE_CORES cores to make and $CORES cores for regression tests"
-export EXE CORES MAKE_CORES
+echo "Using $CORES cores for regression tests"
+export EXE CORES
 
 NUM_FAILS=0
 STDBUF=''
