8f9ea0756008f47d129fae86fcb0113970e58c9e
Voark <fredisawesome147@gmail.com>
2021-04-19 17:22:42 -0700

    fix regression test paths

--- regression-tests/cellviewer/full-test.sh
+++ regression-tests/cellviewer/full-test.sh
@@ -4,27 +4,27 @@ die() { echo "FATAL ERROR: $@" >&2; exit 1;
 npm() { echo "Not running npm, since it seems to be broken" >&2
 }
 echo "Testing cellviewer (pushing to website)"
-npm install --prefix cellviewer || die "npm failed"
+npm install --prefix tools/cellviewer || die "npm failed"
 
-rm -rf ./cellviewer/src/output
-rm -rf ./cellviewer/src/bestfit
+rm -rf ./tools/cellviewer/src/output
+rm -rf ./tools/cellviewer/src/bestfit
 rm -rf node_modules/gh-pages/.cache
-mkdir ./cellviewer/src/output
-mkdir ./cellviewer/src/bestfit
+mkdir ./tools/cellviewer/src/output
+mkdir ./tools/cellviewer/src/bestfit
 
 python3 "src/main.py" \
     --frame_first 0 \
     --frame_last 13 \
     --debug "./debug" \
-    --input "./input/frame%03d.png" \
-    --output "./cellviewer/src/output" \
-    --bestfit "./cellviewer/src/bestfit" \
-    --config "./local_optimizer_config.json" \
-    --initial "./cells.0.csv" || die "main.py failed"
+    --input "./examples/canonical/frame%03d.png" \
+    --output "./tools/cellviewer/src/output" \
+    --bestfit "./tools/cellviewer/src/bestfit" \
+    --config "./examples/canonical/local_optimizer_config.json" \
+    --initial "./examples/canonical/initial.csv" || die "main.py failed"
 
-python3 "tools/cellviewer/radialtree.py" \
-    "./cellviewer/src/output" || die "radialtree failed"
+python3 "./tools/cellviewer/radialtree.py" \
+    "./tools/cellviewer/src/output" || die "radialtree failed"
 
-npm run --prefix cellviewer deploy || die "2nd npm failed"
+npm run --prefix tools/cellviewer deploy || die "2nd npm failed"
 
 echo "Done testing cellviewer (pushed to website)"
