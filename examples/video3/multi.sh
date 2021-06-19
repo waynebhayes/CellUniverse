28b8257a4d698204eef8b926bd3b63e62ebec440
Voark <fredisawesome147@gmail.com>
2021-06-19 11:52:56 -0700

    New configs

---
+++ examples/video3/multi.sh
@@ -0,0 +1,19 @@
+dir=$(pwd)
+cd ../..
+if python "./src/main.py" \
+    --frame_first 22 \
+    --frame_last 200 \
+    --input "$dir/input/%d.jpg" \
+    --bestfit "$dir/output/bestfit" \
+    --output "$dir/output" \
+    --config "$dir/config.json" \
+    --initial "$dir/top_left.csv"  \
+    --continue_from 22 \
+    -ta 0 \
+    -ts 1 \
+    -te 0.000001 \
+    --workers 8 --graySynthetic --global_optimization; then
+    :
+else
+    die "Python quit unexpectedly!"
+fi
