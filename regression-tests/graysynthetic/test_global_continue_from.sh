2d5e796854662a3a0a1b2af8e9da5cfdf4728834
Frederick Dong <53128285+Voark@users.noreply.github.com>
2022-05-21 20:33:27 -0700

    Add continue from regression test
    * initial test
    
    * fix test

---
+++ regression-tests/graysynthetic/test_global_continue_from.sh
@@ -0,0 +1,37 @@
+die() { echo "$@" >&2; exit 1;
+}
+
+echo "Testing simulated annealing global optimization from a continued frame"
+
+[ -d "$REG_DIR" ] || die "Must run from the repository's root directory!"
+
+# create the test output dir if it doesn't exist
+rm -rf $REG_DIR/output
+mkdir $REG_DIR/output
+
+rm -rf $REG_DIR/bestfit
+mkdir $REG_DIR/bestfit
+
+if python3 "src/main.py" \
+    --frame_first 0 \
+    --frame_last 13 \
+    --debug "debug" \
+    --input "examples/canonical/input/gray/frame%03d.png" \
+    --output "$REG_DIR/output" \
+    --bestfit "$REG_DIR/bestfit" \
+    --config "examples/canonical/global_optimizer_config.json" \
+    --initial "examples/canonical/initial.csv"  \
+    --lineage_file "examples/canonical/true_lineage.csv" \
+    --continue_from 5 \
+    -ta 0 \
+    -ts 1 \
+    -te 0.000001 \
+    --no_parallel --global_optimization --graySynthetic; then
+    :
+else
+    die "Python quit unexpectedly!"
+fi
+
+python3 "$REG_DIR/compare.py" "$REG_DIR/output/lineage.csv" "$REG_DIR/expected_lineage.csv" || die "compare failed"
+
+echo "Done testing simulated annealing global optimization."
