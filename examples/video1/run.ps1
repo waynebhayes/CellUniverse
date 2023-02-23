d12529e97b2c1b5482baca7e9efab999610129b7
Chen Song <csong16@uci.edu>
2023-02-23 13:21:48 -0800

    Include guide to run the examples in the README. Update powershell scripts for each examples. Add padding value in the example config files.

--- examples/video1/run.ps1
+++ examples/video1/run.ps1
@@ -2,17 +2,15 @@ $dir=$(Get-Location)
 Set-Location ../..
 python "./src/main.py" `
     --frame_first 0 `
-    --frame_last 30 `
-    --input "$dir/input/gray/frame%03d.png" `
+    --frame_last 350 `
+    --input "$dir/input/%d.png" `
     --bestfit "$dir/output/bestfit" `
     --output "$dir/output" `
-    --config "$dir/global_optimizer_config.json" `
-    --initial "$dir/initial.csv" `
+    --config "$dir/config.json" `
+    --initial "$dir/initial.csv"  `
     --no_parallel --graySynthetic --global_optimization
-
 if($LASTEXITCODE -ne 0)
 {
     Write-Output "Python quit unexpectedly!"
 }
-
 Set-Location $dir
--- examples/video2/run.ps1
+++ examples/video2/run.ps1
@@ -2,17 +2,15 @@ $dir=$(Get-Location)
 Set-Location ../..
 python "./src/main.py" `
     --frame_first 0 `
-    --frame_last 30 `
-    --input "$dir/input/gray/frame%03d.png" `
+    --frame_last 100 `
+    --input "$dir/input/original/%d.png" `
     --bestfit "$dir/output/bestfit" `
     --output "$dir/output" `
-    --config "$dir/global_optimizer_config.json" `
-    --initial "$dir/initial.csv" `
+    --config "$dir/config.json" `
+    --initial "$dir/input/original/initial.csv"  `
     --no_parallel --graySynthetic --global_optimization
 
-if($LASTEXITCODE -ne 0)
-{
+if($LASTEXITCODE -ne 0){
     Write-Output "Python quit unexpectedly!"
 }
-
 Set-Location $dir
