cb7fd2aeefdf2222913cc2acc9e18797dede9814
Yuxuanzzz <93758547+Yuxuanzzz@users.noreply.github.com>
2024-01-22 23:31:41 -0800

    Update Cell.cpp

--- 3d/C++/src/Cells/Cell.cpp
+++ 3d/C++/src/Cells/Cell.cpp
@@ -10,7 +10,6 @@ class CellParams {
     //The CellParams class stores the parameters of a particular cell.
 public:
     std::string name;
-    //CellParams(const std::string& name) : name(name) {} //Krishna
 };
 
 class CellConfig {
@@ -38,18 +37,19 @@ class Cell {
     //The Cell class stores information about a particular cell.
 protected:
     CellParams paramClass;
-    CellConfig cellConfig;
+    CellConfig cellConfig; //?unsure
 public:
     Cell(const CellParams& initProps) : paramClass(initProps) {}
     virtual ~Cell() = default;
 
-    virtual void draw() const = 0;
-    virtual void draw_outline() const = 0;
+    virtual void draw(cv::Mat& image, SimulationConfig simulationConfig, cv::Mat* cellMap = nullptr, float z = 0) const = 0;
+    virtual void draw_outline(cv::Mat& image, cv::Scalar color, float z = 0) const = 0;
     virtual Cell* get_perturbed_cell() const = 0;
     virtual Cell* get_paramaterized_cell(std::unordered_map<std::string, float> params = {}) const = 0;
-    virtual std::tuple<Cell*, Cell*, bool> get_split_cells() const = 0;
+    virtual std::tuple<Cell, Cell, bool> get_split_cells() const = 0;
     virtual CellParams get_cell_params() const = 0;
     static bool check_if_cells_valid(const std::vector<Cell*>& cells) { return false; }
     virtual std::tuple<std::vector<float>, std::vector<float>> calculate_corners() const = 0;
     virtual std::tuple<std::vector<float>, std::vector<float>> calculate_minimum_box(const Cell& perturbed_cell) const = 0;
 };
+
