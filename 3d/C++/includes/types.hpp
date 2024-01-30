935e5c678a95ad2b898555c7f71afdc186d63d2c
Tim <timyuan1016@gmail.com>
2024-01-30 12:14:50 -0800

    Refactored the project structure.

---
+++ 3d/C++/includes/types.hpp
@@ -0,0 +1,13 @@
+//
+// Created by yuant on 1/30/2024.
+//
+
+#ifndef CELLUNIVERSE_TYPES_HPP
+#define CELLUNIVERSE_TYPES_HPP
+
+#include <string>
+
+typedef std::string Path;
+
+
+#endif //CELLUNIVERSE_TYPES_HPP
