a4296fe6ada5661c9dbc15b951fbd143d0f81688
Faisal Alshaiddi <falshaid@uci.edu>
2024-02-12 22:53:57 -0800

    added draw/draw_outline functions

--- 3d/C++/src/Cells/Bacilli.cpp
+++ 3d/C++/src/Cells/Bacilli.cpp
@@ -25,22 +25,132 @@ void Bacilli::_refresh() {
     _needs_refresh = false;
 }
 
-void Bacilli::draw() {
+vvoid Bacilli::draw(cv::Mat& image, cv::Mat& cell_map, bool is_cell, cv::Scalar background_color, cv::Scalar cell_color, std::map<std::string, std::string> simulation_config) {
     if (dormant)
         return;
 
     if (_needs_refresh)
         _refresh();
 
-    // Implement the draw function
+    cv::Mat mask = cv::Mat::zeros(image.size(), CV_8UC1);
+
+    int top = _region.top;
+    int bottom = _region.bottom;
+    int left = _region.left;
+    int right = _region.right;
+    int width = right - left;
+    int height = bottom - top;
+
+    cv::Mat body_mask = cv::Mat::zeros(mask.size(), CV_8UC1);
+    cv::fillPoly(body_mask, std::vector<std::vector<cv::Point>>{{cv::Point(_head_left.x - left, _head_left.y - top),
+        cv::Point(_head_right.x - left, _head_right.y - top),
+        cv::Point(_tail_right.x - left, _tail_right.y - top),
+        cv::Point(_tail_left.x - left, _tail_left.y - top)}}, cv::Scalar(255));
+    cv::Mat body_mask_up = cv::Mat::zeros(mask.size(), CV_8UC1);
+    cv::fillPoly(body_mask_up, std::vector<std::vector<cv::Point>>{{cv::Point(_head_left.x - _region.left, _head_left.y - _region.top),
+        cv::Point((std::ceil((_head_right.x + _head_left.x) / 2) - _region.left), (std::ceil((_head_right.y + _head_left.y) / 2) - _region.top)),
+        cv::Point((std::ceil((_tail_right.x + _tail_left.x) / 2) - _region.left), (std::ceil((_tail_right.y + _tail_left.y) / 2) - _region.top)),
+        cv::Point(_tail_left.x - _region.left, _tail_left.y - _region.top)}}, cv::Scalar(255));
+    cv::Mat body_mask_middle = cv::Mat::zeros(mask.size(), CV_8UC1);
+    cv::fillPoly(body_mask_middle, std::vector<std::vector<cv::Point>>{{cv::Point((std::ceil((_head_right.x + _head_left.x * 2) / 3) - _region.left),
+        (std::ceil((_head_right.y + _head_left.y * 2) / 3) - _region.top)),
+        cv::Point((std::ceil((_head_right.x * 2 + _head_left.x) / 3) - _region.left),
+            (std::ceil((_head_right.y * 2 + _head_left.y) / 3) - _region.top)),
+        cv::Point((std::ceil((_tail_right.x * 2 + _tail_left.x) / 3) - _region.left),
+            (std::ceil((_tail_right.y * 2 + _tail_left.y) / 3) - _region.top)),
+        cv::Point((std::ceil((_tail_right.x + _tail_left.x * 2) / 3) - _region.left),
+            (std::ceil((_tail_right.y + _tail_left.y * 2) / 3) - _region.top))}}, cv::Scalar(255));
+    cv::Mat head_mask = cv::Mat::zeros(mask.size(), CV_8UC1);
+    cv::circle(head_mask, cv::Point(_head_center.x - left, _head_center.y - top), _width / 2, cv::Scalar(255), -1);
+    cv::Mat tail_mask = cv::Mat::zeros(mask.size(), CV_8UC1);
+    cv::circle(tail_mask, cv::Point(_tail_center.x - left, _tail_center.y - top), _width / 2, cv::Scalar(255), -1);
+
+    try {
+        std::string image_type = simulation_config["image.type"];
+        double diffraction_strength = std::stod(simulation_config["light.diffraction.strength"]);
+        double cell_opacity = (_opacity != "auto" && _opacity != "None" && _opacity != "") ? std::stod(_opacity) : std::stod(simulation_config["cell.opacity"]);
+
+        if (image_type == "phaseContrast") {
+            if (!is_cell) {
+                mask.setTo(0);
+                body_mask.copyTo(mask);
+                head_mask.copyTo(mask);
+                tail_mask.copyTo(mask);
+                image(_region)[mask] = 0.39 * 255;  // 0.39 * 255 = 100
+            }
+            else {
+                mask.setTo(0);
+                body_mask.copyTo(mask);
+                image(_region)[mask] = 0.25 * 255;  // 0.25 * 255 = 65
+
+                mask.setTo(0);
+                head_mask.copyTo(mask);
+                image(_region)[mask] = 0.25 * 255;
+
+                mask.setTo(0);
+                tail_mask.copyTo(mask);
+                image(_region)[mask] = 0.25 * 255;
+
+                mask.setTo(0);
+                body_mask_up.copyTo(mask);
+                image(_region)[mask] = 0.63 * 255;  // 0.63 * 255 = 160
+
+                mask.setTo(0);
+                body_mask_middle.copyTo(mask);
+                image(_region)[mask] = 0.39 * 255;  // 0.39 * 255 = 100
+            }
+        }
+        else if (image_type == "graySynthetic") {
+            mask.setTo(0);
+            body_mask.copyTo(mask);
+            head_mask.copyTo(mask);
+            tail_mask.copyTo(mask);
+            image(_region)[mask] += diffraction_strength;
+
+            cv::Mat diffraction_mask = cv::Mat::zeros(image.size(), CV_8UC1);
+            diffraction_mask.setTo(background_color);
+            cell_map(_region)[mask] += 1;
+            cv::GaussianBlur(cell_map, diffraction_mask, cv::Size(0, 0), std::stod(simulation_config["light.diffraction.sigma"]));
+            diffraction_mask.setTo(cell_color + cell_opacity * diffraction_mask);
+            image(_region) = diffraction_mask;
+        }
+        else if (image_type == "binary") {
+            mask.setTo(0);
+            body_mask.copyTo(mask);
+            head_mask.copyTo(mask);
+            tail_mask.copyTo(mask);
+            if (is_cell) {
+                image(_region)[mask] += 1.0;
+                cell_map(_region)[mask] += 1;
+            }
+            else {
+                image(_region)[mask] -= 1.0;
+                cell_map(_region)[mask] -= 1;
+            }
+        }
+    }
+    catch (std::exception& e) {
+        dormant = true;
+    }
 }
 
-void Bacilli::draw_outline() {
+void Bacilli::draw_outline(cv::Mat& image, cv::Scalar color) {
     if (_needs_refresh)
         _refresh();
 
-    // Implement the draw_outline function
+    cv::line(image, cv::Point(_tail_left.x, _tail_left.y), cv::Point(_head_left.x, _head_left.y), color);
+    cv::line(image, cv::Point(_tail_right.x, _tail_right.y), cv::Point(_head_right.x, _head_right.y), color);
+
+    double t0 = std::atan2(_head_right.y - _head_center.y, _head_right.x - _head_center.x);
+    double t1 = std::atan2(_head_left.y - _head_center.y, _head_left.x - _head_center.x);
+    cv::ellipse(image, cv::Point(_head_center.x, _head_center.y), cv::Size(_width / 2, _width / 2), 0, t0, t1, color);
+
+    t0 = std::atan2(_tail_right.y - _tail_center.y, _tail_right.x - _tail_center.x);
+    t1 = std::atan2(_tail_left.y - _tail_center.y, _tail_left.x - _tail_center.x);
+    cv::ellipse(image, cv::Point(_tail_center.x, _tail_center.y), cv::Size(_width / 2, _width / 2), 0, t0, t1, color);
 }
+
+
 
 std::pair<Bacilli*, Bacilli*> Bacilli::split(double alpha) {
     if (_needs_refresh)
