b2e07617fdc7021cb8d4136b3865eb8831a2ec75
Jose <josegonz115@gmail.com>
2024-10-04 19:14:27 -0700

    updated paths in 2d README files

---
+++ 3d/examples/runcpp.sh
@@ -0,0 +1,8 @@
+dir=$(pwd)
+cd ..
+if  ./C++/build/celluniverse 1 9 $dir/input/frame%03d.tif $dir/output $dir/config.yaml $dir/initial.csv
+    then
+    :
+else
+    echo "Cell Universe quit unexpectedly!"
+fi
