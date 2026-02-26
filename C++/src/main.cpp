02832bc5511aff3644032525981b30c94666c418
QAQWillQwQ <13165315556@163.com>
2026-02-25 16:06:02 -0800

    fix main

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -211,10 +211,10 @@ int main(int argc, char *argv[])
     CellFactory cellFactory(config);
     std::map<Path, std::vector<Spheroid>> cells = cellFactory.createCells(args.initial, config.simulation.z_slices / 2,
                                                                         config.simulation.z_scaling);
-    // create lineage here
-    Lineage lineage = Lineage(cells, imageFilePaths, config, args.output, args.firstFrame, args.continueFrom);
+    // // create lineage here
+    // Lineage lineage = Lineage(cells, imageFilePaths, config, args.output, args.firstFrame, args.continueFrom);
     // create lineage
-    //Lineage lineage = Lineage(cells, imageFilePaths, config, args.output, args.continueFrom);
+    Lineage lineage = Lineage(cells, imageFilePaths, config, args.output, args.continueFrom);
 
     LineageViewer viewer;
 
